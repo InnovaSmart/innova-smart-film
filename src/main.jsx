19e80ce105f572d0a894c904af251fe5f058d3b2
InnovaSmart <innovasmartfilm@gmail.com>
2025-06-20 19:05:51 -0400

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import SmartFilmPage from '../SmartFilmPage';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SmartFilmPage />
-  </React.StrictMode>
-);
